301b460289d8f84c91113d8d8d860370dd8431ab
Chebbi_raed <61346201+ROPOROS@users.noreply.github.com>
2026-01-31 14:14:31 +0100

    chore(cleanup): remove unused files and add root .gitignore

--- client/src/context/useSocket.ts
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { SocketContext } from "./SocketContext";
-
-export const useSocket = () => {
-  const context = useContext(SocketContext);
-  if (!context) throw new Error("useSocket must be used within SocketProvider");
-  return context;
-};

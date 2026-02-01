765a6e647ec4a24a547278f8f634d45e4bd8dee0
Chebbi_raed <61346201+ROPOROS@users.noreply.github.com>
2026-02-01 09:11:08 +0100

    refactor(client): use type import for interfaces

--- client/src/context/SocketContext.tsx
+++ client/src/context/SocketContext.tsx
@@ -2,7 +2,7 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
 import { io, Socket } from 'socket.io-client';
 import { useNavigate } from 'react-router-dom';
-import { Session, Location } from '../types';
+import type { Session, Location } from '../types';
 
 interface SocketContextType {
     socket: Socket | null;
